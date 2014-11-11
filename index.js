81fa9b733c8b8005389a3ed6dc780f3ba1e95c3c
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-11-11 17:04:41 +0100

    Simplify.

--- index.js
+++ index.js
@@ -2,24 +2,16 @@
 
 
 exports.yamlFile2Json = function (yamlFile) {
-
   var yaml = require ('js-yaml');
   var fs   = require ('fs');
 
   var data = fs.readFileSync (yamlFile, 'utf8');
-
-  var def = yaml.safeLoad (data);
-
-  return def;
+  return yaml.safeLoad (data);
 };
 
 exports.jsonFile2Json = function (jsonFile) {
-
   var fs   = require ('fs');
 
   var data = fs.readFileSync (jsonFile, 'utf8');
-
-  var r = JSON.parse (data);
-
-  return r;
+  return JSON.parse (data);
 };
