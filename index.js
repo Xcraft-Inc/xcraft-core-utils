8852bdee60f7dd494d58b30ebbaa1e618348325d
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-04-03 11:40:48 +0200

    Factorize a require.

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 'use strict';
 
+var fs = require ('fs');
+
 
 exports.yamlFile2Json = function (yamlFile) {
   var yaml = require ('js-yaml');
-  var fs   = require ('fs');
 
   var data = fs.readFileSync (yamlFile, 'utf8');
   return yaml.safeLoad (data);
 };
 
 exports.jsonFile2Json = function (jsonFile) {
-  var fs   = require ('fs');
-
   var data = fs.readFileSync (jsonFile, 'utf8');
   return JSON.parse (data);
 };
 
 exports.json2JsonFile = function (json, destFile) {
-  var fs   = require ('fs');
   fs.writeFileSync (destFile, JSON.stringify (json), 'utf8');
 };
 
