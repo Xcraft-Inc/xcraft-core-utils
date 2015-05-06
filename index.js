1f550b6bfa00c97414b21fac5e3aae680a225046
Samuel Loup <samuel.loup@gmail.com>
2015-05-06 14:30:20 +0200

    Extract token generation for xutils

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var fs = require ('fs');
-
+var fs     = require ('fs');
+var crypto = require ('crypto');
 
 exports.yamlFile2Json = function (yamlFile) {
   var yaml = require ('js-yaml');
@@ -26,9 +26,33 @@ exports.topic2Action = function (topic) {
 };
 
 exports.md5 = function (data) {
-  var crypto = require ('crypto');
-
   var md5 = crypto.createHash ('md5');
   md5.update (data);
   return md5.digest ('hex');
+};
+
+exports.generateToken = function (callback) {
+  var createKey = function (key) {
+    var shasum = crypto.createHash ('sha1');
+    shasum.update (key);
+    return shasum.digest ('hex');
+  };
+
+  var buf = null;
+
+  try {
+    buf = crypto.randomBytes (256);
+    callback (null, createKey (buf));
+  } catch (ex) {
+    /* Handle error.
+     * Most likely, entropy sources are drained.
+     */
+    crypto.pseudoRandomBytes (256, function (ex, buf) {
+      if (ex) {
+        throw ex;
+      }
+
+      callback (null, createKey (buf));
+    });
+  }
 };
