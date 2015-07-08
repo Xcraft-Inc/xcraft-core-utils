7cb2d4be5432ca59f57ceb53921a04d06e3201eb
Mathieu Schroeter <schroeter@epsitec.ch>
2015-07-08 09:10:30 +0200

    Move crypto functions in a dedicated file.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var fs     = require ('fs');
-var crypto = require ('crypto');
 
 exports.yamlFile2Json = function (yamlFile) {
   var yaml = require ('js-yaml');
@@ -25,36 +24,5 @@ exports.topic2Action = function (topic) {
   });
 };
 
-exports.md5 = function (data) {
-  var md5 = crypto.createHash ('md5');
-  md5.update (data);
-  return md5.digest ('hex');
-};
-
-exports.generateToken = function (callback) {
-  var createKey = function (key) {
-    var shasum = crypto.createHash ('sha1');
-    shasum.update (key);
-    return shasum.digest ('hex');
-  };
-
-  var buf = null;
-
-  try {
-    buf = crypto.randomBytes (256);
-    callback (null, createKey (buf));
-  } catch (ex) {
-    /* Handle error.
-     * Most likely, entropy sources are drained.
-     */
-    crypto.pseudoRandomBytes (256, function (ex, buf) {
-      if (ex) {
-        throw ex;
-      }
-
-      callback (null, createKey (buf));
-    });
-  }
-};
-
+exports.crypto = require ('./lib/crypto.js');
 exports.string = require ('./lib/string.js');
