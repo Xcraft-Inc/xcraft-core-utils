55121714fe27a4b952bd56df0f6fb7b24b96f84e
Mathieu Schroeter <schroeter@epsitec.ch>
2015-07-08 09:25:42 +0200

    Move yaml stuff in a dedicated file.

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 'use strict';
 
-var fs     = require ('fs');
-
-exports.yamlFile2Json = function (yamlFile) {
-  var yaml = require ('js-yaml');
-
-  var data = fs.readFileSync (yamlFile, 'utf8');
-  return yaml.safeLoad (data);
-};
-
 exports.topic2Action = function (topic) {
   return topic.replace (/(\.[a-z])/g, function (match) {
     return match.replace ('.', '').toUpperCase ();
@@ -18,3 +9,4 @@ exports.topic2Action = function (topic) {
 exports.crypto = require ('./lib/crypto.js');
 exports.json   = require ('./lib/json.js');
 exports.string = require ('./lib/string.js');
+exports.yaml   = require ('./lib/yaml.js');
