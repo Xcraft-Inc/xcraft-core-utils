73c67304ed58bcc7f83edacca99066e99ead8016
Mathieu Schroeter <schroeter@epsitec.ch>
2015-07-24 14:33:13 +0200

    Move the output log code in a more generic function.
    Then this function will be useful for the GitLab CI runner too.

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 
 exports.crypto = require ('./lib/crypto.js');
 exports.json   = require ('./lib/json.js');
+exports.log    = require ('./lib/log.js');
 exports.string = require ('./lib/string.js');
 exports.yaml   = require ('./lib/yaml.js');
