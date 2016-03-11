faf9d3be924dcdd4a819f90f57dd8947a071bf2f
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-03-11 19:10:13 +0100

    Move toRegexp in the xUtils module

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ exports.batch  = require ('./lib/batch.js');
 exports.crypto = require ('./lib/crypto.js');
 exports.json   = require ('./lib/json.js');
 exports.log    = require ('./lib/log.js');
+exports.regex  = require ('./lib/regex.js');
 exports.string = require ('./lib/string.js');
 exports.yaml   = require ('./lib/yaml.js');
