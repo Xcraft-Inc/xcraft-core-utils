bbc6226e2cc080ccb4a7df1d2cf37c0366f72cb1
Mathieu Schroeter <schroeter@epsitec.ch>
2017-06-12 13:23:43 +0200

    Add helper for reflection on function arguments

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ exports.batch = require ('./lib/batch.js');
 exports.crypto = require ('./lib/crypto.js');
 exports.json = require ('./lib/json.js');
 exports.log = require ('./lib/log.js');
+exports.reflect = require ('./lib/reflect.js');
 exports.regex = require ('./lib/regex.js');
 exports.string = require ('./lib/string.js');
 exports.yaml = require ('./lib/yaml.js');
