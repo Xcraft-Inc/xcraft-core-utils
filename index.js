846d14841712b33599412e928286bc924b79474f
Mathieu Schroeter <mathieu@schroetersa.ch>
2018-02-21 00:09:17 +0100

    Update xcraft-dev-rules to v0.2 and prettify everything

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-exports.batch = require ('./lib/batch.js');
-exports.crypto = require ('./lib/crypto.js');
-exports.json = require ('./lib/json.js');
-exports.log = require ('./lib/log.js');
-exports.os = require ('./lib/os.js');
-exports.reflect = require ('./lib/reflect.js');
-exports.regex = require ('./lib/regex.js');
-exports.string = require ('./lib/string.js');
-exports.yaml = require ('./lib/yaml.js');
+exports.batch = require('./lib/batch.js');
+exports.crypto = require('./lib/crypto.js');
+exports.json = require('./lib/json.js');
+exports.log = require('./lib/log.js');
+exports.os = require('./lib/os.js');
+exports.reflect = require('./lib/reflect.js');
+exports.regex = require('./lib/regex.js');
+exports.string = require('./lib/string.js');
+exports.yaml = require('./lib/yaml.js');
