a033740188a4a17ebd0453985b770638df5ef3a7
Mathieu Schroeter <mathieu@schroetersa.ch>
2018-11-02 14:40:05 +0100

    Add helpers for testing if an object is a function or generator

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 exports.batch = require('./lib/batch.js');
 exports.crypto = require('./lib/crypto.js');
 exports.SQLite = require('./lib/sqlite.js');
+exports.js = require('./lib/js.js');
 exports.json = require('./lib/json.js');
 exports.locks = require('./lib/locks.js');
 exports.log = require('./lib/log.js');
