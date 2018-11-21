0969b405f6e9b661e413a659860d772b4bd4d51d
Mathieu Schroeter <mathieu@schroetersa.ch>
2018-11-21 22:58:40 +0100

    Cosmetic

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 exports.batch = require('./lib/batch.js');
 exports.crypto = require('./lib/crypto.js');
-exports.SQLite = require('./lib/sqlite.js');
 exports.js = require('./lib/js.js');
 exports.json = require('./lib/json.js');
 exports.locks = require('./lib/locks.js');
@@ -13,4 +12,5 @@ exports.regex = require('./lib/regex.js');
 exports.string = require('./lib/string.js');
 exports.yaml = require('./lib/yaml.js');
 
+exports.SQLite = require('./lib/sqlite.js');
 exports.RankedCache = require('./lib/ranked-cache.js');
