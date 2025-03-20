78436b391ce0b5a10ef0f3f20516da918cc54baf
Samuel Loup <samuel.loup@gmail.com>
2025-03-20 09:49:12 +0100

    Provide new utilities writed by Mathieu Schroeter

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 exports.ArrayCollector = require('./lib/arrayCollector.js');
 exports.batch = require('./lib/batch.js');
+exports.Batcher = require('./lib/batcher.js');
 exports.crypto = require('./lib/crypto.js');
 exports.EventDebouncer = require('./lib/eventDebouncer.js');
 exports.files = require('./lib/files.js');
 exports.fileCrypto = require('./lib/file-crypto.js');
+exports.hash = require('./lib/hash.js');
 exports.js = require('./lib/js.js');
 exports.json = require('./lib/json.js');
 exports.locks = require('./lib/locks.js');
@@ -16,10 +18,11 @@ exports.os = require('./lib/os.js');
 exports.propTypes = require('./lib/prop-types.js');
 exports.reflect = require('./lib/reflect.js');
 exports.regex = require('./lib/regex.js');
+exports.RestAPI = require('./lib/rest.js');
 exports.string = require('./lib/string.js');
 exports.whereIs = require('./lib/whereIs.js');
 exports.yaml = require('./lib/yaml.js');
-
+exports.zippy = require('./lib/zippy.js');
 exports.RankedCache = require('./lib/ranked-cache.js');
 exports.JobQueue = require('./lib/job-queue.js');
 exports.CursorPump = require('./lib/cursorPump.js');
---
+++ test/hash.spec.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const {expect} = require('chai');
+const {computeHash} = require('../lib/utils/hash.js');
+
+describe('goblin.yennefer.hash', function () {
+  it('string', function () {
+    const objL = {string: 'Roger'};
+    const objR = {string: 'Wilco'};
+
+    const L = computeHash(objL);
+    const R = computeHash(objR);
+
+    expect(L).to.not.be.equal(R);
+  });
+
+  it('number', function () {
+    const objL = {number: 42};
+    const objR = {number: 1548};
+
+    const L = computeHash(objL);
+    const R = computeHash(objR);
+
+    expect(L).to.not.be.equal(R);
+  });
+
+  it('boolean', function () {
+    const objL = {boolean: true};
+    const objR = {boolean: false};
+
+    const L = computeHash(objL);
+    const R = computeHash(objR);
+
+    expect(L).to.not.be.equal(R);
+  });
+
+  it('empty', function () {
+    const objL = {empty: ''};
+    const objR = {empty: null};
+
+    const L = computeHash(objL);
+    const R = computeHash(objR);
+
+    expect(L).to.not.be.equal(R);
+  });
+});
